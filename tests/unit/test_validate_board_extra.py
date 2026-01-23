dfd1ea500218af79ba214cd41c765a2718e9ee1f
Kin Chung Choy <mkcchoy@student.ubc.ca>
2026-01-23 12:14:55 -0800

    Add 4 new unit tests

---
+++ tests/unit/test_validate_board_extra.py
@@ -0,0 +1,39 @@
+'''
+Add 4 unit test to trigger checks
+'''
+import pytest
+from sudoku_validation.row_validation import row_validation
+
+def test_validate_rows_valid():
+    board = [
+        [1,2,3,4,5,6,7,8,9],
+        [4,5,6,7,8,9,1,2,3],
+        [7,8,9,1,2,3,4,5,6],
+        [2,3,4,5,6,7,8,9,1],
+        [5,6,7,8,9,1,2,3,4],
+        [8,9,1,2,3,4,5,6,7],
+        [3,4,5,6,7,8,9,1,2],
+        [6,7,8,9,1,2,3,4,5],
+        [9,1,2,3,4,5,6,7,8],
+    ]
+    assert row_validation(board) is True
+
+def test_validate_rows_duplicate():
+    board = [
+        [1,1,3,4,5,6,7,8,9],  # duplicate 1
+    ] + [[0]*9 for _ in range(8)]
+    assert row_validation(board) is False
+
+def test_validate_rows_non_numeric():
+    board = [
+        ["a",2,3,4,5,6,7,8,9],
+    ] + [[0]*9 for _ in range(8)]
+    with pytest.raises(ValueError):
+        row_validation(board)
+
+def test_validate_rows_short_row():
+    board = [
+        [1,2,3],  # too short
+    ] + [[0]*9 for _ in range(8)]
+    with pytest.raises(ValueError):
+        row_validation(board)
